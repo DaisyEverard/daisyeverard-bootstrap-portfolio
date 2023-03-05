947b007e37bdaaed7cd0a2630e118a3f84c7cb9b
DaisyEverard <msdaisyeverard@gmail.com>
2023-03-05 14:00:07 +0000

    update projects

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,40 +11,41 @@ const card22Img = card22.parentElement.querySelector('.image-container');
 const card31Img = card31.parentElement.querySelector('.image-container');
 const card32Img = card32.parentElement.querySelector('.image-container');
 const card33Img = card33.parentElement.querySelector('.image-container');
+
 // card-1-1
-card11.querySelector('.card-title').textContent = 'Day Planner'; 
-card11.querySelector('.card-text').textContent = 'Event planner for 9-5 hour timeblocks'; 
-card11.querySelector('a').setAttribute('href', 'https://daisyeverard.github.io/Work-Day-Planner/'); 
-card11Img.setAttribute('alt', 'pile of notebooks')
-card11Img.style.backgroundImage = "url(./assets/images/notebooks.jpg)"; 
+card11.querySelector('.card-title').textContent = 'Cocktail Search'; 
+card11.querySelector('.card-text').textContent = 'Get cocktail recipes based on a name, ingredient, or category'; 
+card11.querySelector('a').setAttribute('href', 'https://daisyeverard.github.io/Cocktail-Search/'); 
+card11Img.setAttribute('alt', '3 cocktails on a wooden bar')
+card11Img.style.backgroundImage = "url(./assets/images/three-cocktails.jpg)";
 
 // card-2-1
-card21.querySelector('.card-title').textContent = 'Code Quiz'; 
-card21.querySelector('.card-text').textContent = 'Quiz about code that stores the top 3 scores'; 
-card21.querySelector('a').setAttribute('href', 'https://daisyeverard.github.io/Code-Quiz/'); 
-card21Img.setAttribute('alt', 'alt-here')
-card21Img.style.backgroundImage = "url(./assets/images/quiz-bg.jpg)"; 
+card21.querySelector('.card-title').textContent = 'Weather Dashboard'; 
+card21.querySelector('.card-text').textContent = 'Get todays weather and a 5 day forecast for any city'; 
+card21.querySelector('a').setAttribute('href', 'https://daisyeverard.github.io/Weather-Dashboard/'); 
+card21Img.setAttribute('alt', 'black and white cloudy sky with person')
+card21Img.style.backgroundImage = "url(./assets/images/bw-clouds.jpg)"; 
 
 // card-2-2
-card22.querySelector('.card-title').textContent = 'Password Generator'; 
-card22.querySelector('.card-text').textContent = 'Custom random password generator'; 
-card22.querySelector('a').setAttribute('href', 'https://daisyeverard.github.io/Password-Generator/'); 
-card22Img.setAttribute('alt', 'alt-here')
-card22Img.style.backgroundImage = "url(./assets/images/password-bg.jpg)"; 
+card22.querySelector('.card-title').textContent = 'Team Profile Generator'; 
+card22.querySelector('.card-text').textContent = 'Node.js HTML profile page generator'; 
+card22.querySelector('a').setAttribute('href', 'https://github.com/DaisyEverard/Team-Profile-Generator'); 
+card22Img.setAttribute('alt', '5 people with hands in circle')
+card22Img.style.backgroundImage = "url(./assets/images/team-hands.jpg)"; 
 
 // card-3-1
-card31.querySelector('.card-title').textContent = 'JS Financial Analysis'; 
-card31.querySelector('.card-text').textContent = 'Uses the console and Javscript to analyse a set of financial data'; 
-card31.querySelector('a').setAttribute('href', 'https://daisyeverard.github.io/console-financial-analysis/'); 
-card31Img.setAttribute('alt', 'Dark computer screen with analysis')
-card31Img.style.backgroundImage = "url(./assets/images/financial-bg.jpg)"; 
+card31.querySelector('.card-title').textContent = 'Skittles Scores'; 
+card31.querySelector('.card-text').textContent = 'App for skittles teams to automatically calculate points'; 
+card31.querySelector('a').setAttribute('href', 'https://daisyeverard.github.io/Skittles-Scores/'); 
+card31Img.setAttribute('alt', 'Wooden bowling pins')
+card31Img.style.backgroundImage = "url(./assets/images/skittles.jpg)";  
 
 // card-3-2
-card32.querySelector('.card-title').textContent = 'JS Rock Paper Scissors'; 
-card32.querySelector('.card-text').textContent = 'A game of rock, paper, scissors where a user plays the computer. Built with JavaScript'; 
-card32.querySelector('a').setAttribute('href', 'https://daisyeverard.github.io/rockPaperScissors-JSonly/'); 
-card32Img.setAttribute('alt', 'Hand Throwing Rock')
-card32Img.style.backgroundImage = "url(./assets/images/rps-bg.jpg)"; 
+card32.querySelector('.card-title').textContent = 'Day Planner'; 
+card32.querySelector('.card-text').textContent = 'Event planner for 9-5 hour timeblocks'; 
+card32.querySelector('a').setAttribute('href', 'https://daisyeverard.github.io/Work-Day-Planner/'); 
+card32Img.setAttribute('alt', 'pile of notebooks')
+card32Img.style.backgroundImage = "url(./assets/images/notebooks.jpg)"; 
 
 // card-3-3
 card33.querySelector('.card-title').textContent = 'Travel Site'; 
